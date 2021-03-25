60d8eae693b32e01c13af5c660700d12bc9eeb9d
Jotak1 <76694359+Jotak1@users.noreply.github.com>
2021-03-24 23:21:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'https://Jotak1.github.io/'],
+    originWhitelist: ['http://localhost:3000', 'https://worldmapholidayjotak1-c395a8.netlify.app/'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
